db47370f31830890c0db7995f8cd09f897245441
Michael Zavarella <zavarella.dev@gmail.com>
2022-02-15 15:03:53 -0500

    Fix conjure and cmp mappings

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -1,8 +1,8 @@
 local cmp = require('cmp')
 cmp.setup({
     mapping = {
-        ['<C-p>'] = cmp.mapping.select_prev_item(),
-        ['<C-n>'] = cmp.mapping.select_next_item(),
+        ['<C-k>'] = cmp.mapping.select_prev_item(),
+        ['<C-j>'] = cmp.mapping.select_next_item(),
         ['<C-d>'] = cmp.mapping.scroll_docs(-4),
         ['<C-f>'] = cmp.mapping.scroll_docs(4),
         ['<C-Space>'] = cmp.mapping.complete(),
